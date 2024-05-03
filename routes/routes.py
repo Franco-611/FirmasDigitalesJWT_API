bf7681145993daca4e3d70467937aa133917ac81
Franco-611 <69815571+Franco-611@users.noreply.github.com>
2024-05-02 23:22:17 -0600

    cambios

--- routes/routes.py
+++ routes/routes.py
@@ -18,4 +18,31 @@ except Exception as error:
     print(f"No se pudo conectar a la base de datos debido a: {error}")
     connection = None
 
+@routes.route('/usuarios', methods=['GET'])
+def get_users():
+    if connection:
+        cursor = connection.cursor()
+        # Ejecuta la consulta para obtener todos los usuarios
+        cursor.execute("SELECT * FROM usuarios;")  # Cambiado para seleccionar solo las columnas necesarias
 
+        # Fetchall devuelve una lista de tuplas, convertimos cada tupla a un dict
+        users = [{'usuario': row[0], 'contrasena': row[1]} for row in cursor.fetchall()]
+        cursor.close()
+        return jsonify(users)
+    else:
+        return jsonify({'error': 'Error al conectar con la base de datos'}), 500
+    
+
+@routes.route('/canciones', methods=['GET'])
+def get_canciones():
+    if connection:
+        cursor = connection.cursor()
+        # Ejecuta la consulta para obtener todos los usuarios
+        cursor.execute("SELECT * FROM canciones;")  # Cambiado para seleccionar solo las columnas necesarias
+
+        # Fetchall devuelve una lista de tuplas, convertimos cada tupla a un dict
+        users = [{'nombre': row[0], 'info': row[1]} for row in cursor.fetchall()]
+        cursor.close()
+        return jsonify(users)
+    else:
+        return jsonify({'error': 'Error al conectar con la base de datos'}), 500
